dab60f3947bcb208c009bfcbf7f999cca8d4ae7d
Shree Varshan <shreevarshan822@gmail.com>
2026-07-13 20:54:42 +0530

    update: adapters __init__.py added agent adapter

--- src/lily_agent/adapters/__init__.py
+++ src/lily_agent/adapters/__init__.py
@@ -1,5 +1,6 @@
 from .integrations.ollama_adapter import OllamaAdapter
 from .integrations.groq_adapter import GroqAdapter
+from .adapter import AgentAdapter
 
 
-__all__ = ["OllamaAdapter", "GroqAdapter"]
+__all__ = ["OllamaAdapter", "GroqAdapter", "AgentAdapter"]
